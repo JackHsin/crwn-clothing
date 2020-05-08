d3006014fa6965e01296d454827c08d7c3ad3c8e
Jack Hsin <qk14000@gmail.com>
2020-05-08 16:24:35 +0800

    Ch249 Addng performance with  Error Boundary Component for Lazy loading Suspense Use React.Memo for cart-item

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -6,6 +6,8 @@ import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
 import Spinner from '../../components/spinner/spinner.component';
 
+import { ShopPageContainer } from './shop.styles';
+
 const CollectionsOverviewContainer = lazy(() => import('../../components/collections-overview/collections-overview.container'));
 const CollectionPageContainer = lazy (() => import('../collection/collection.container'));
 
@@ -16,12 +18,12 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
     },[fetchCollectionsStart]);
 
     return (
-        <div className='shop-page'>
-        <Suspense fallback={<Spinner />}>
-            <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
-            <Route path={`${match.path}/:collectionId`}  component={CollectionPageContainer} />
-        </Suspense>
-        </div>
+        <ShopPageContainer>
+            <Suspense fallback={<Spinner />}>
+                <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
+                <Route path={`${match.path}/:collectionId`}  component={CollectionPageContainer} />
+            </Suspense>
+        </ShopPageContainer>
     )
 };
 
